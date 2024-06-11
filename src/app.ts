b1242f18ba7bf1aa4986e57fd77f781502f506d3
Ahmed Zeyad <keyboord01@gmail.com>
2024-06-11 04:35:29 +0300

    CORS fallback

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import authRoutes from "./routes/authRoutes";
 import classRoutes from "./routes/classRoutes";
 
 const app = express();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.PUBLIC_API_HOST,
+  })
+);
 
+app.use(bodyParser.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/classes", classRoutes);
 

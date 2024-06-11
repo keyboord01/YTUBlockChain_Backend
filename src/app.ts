5a940e3f5f5f14483550bf7154b409b350d1d1bc
Ahmed Zeyad <keyboord01@gmail.com>
2024-06-12 01:55:06 +0300

    test auto deployment

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/classes", classRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello, Blockchain World! 🌍🚀🚀");
+  res.send("Hello, Blockchain World! 🌍🚀");
 });
 
 export default app;
